b51b07bdefeb542ae771759c8c083513ea403ab2
Rorm <rafael.orm@outlook.com.br>
2024-08-19 15:27:23 -0300

    Até aula 29

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', status=201, context={
+    return render(request, 'recipes/pages/home.html', status=201, context={
                   'name': 'RORM',})
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
